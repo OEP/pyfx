734e6c795261c168123d6585f3ff4f0423e05221
Paul Kilgo <paulkilgo@gmail.com>
2013-05-15 23:39:48 -0400

    work on getting some of these undefined references cleared up

--- src/PerlinNoise.cpp
+++ src/PerlinNoise.cpp
@@ -1,1 +1,14 @@
+#include "PerlinNoise.h"
+using namespace vr;
 
+const float
+PerlinNoise::eval(const Vector &p) const
+{
+  return 0.0f;
+}
+
+const float
+PerlinNoise::getMax() const
+{
+  return 1.1f;
+}
